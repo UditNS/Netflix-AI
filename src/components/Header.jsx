2f80d28dd7ac0083ebc287e9761627056b3341cd
Udit Narayan Singh <uditn007@gmail.com>
2025-09-11 12:57:54 +0530

    ADDED CONSTANT.js to have all the hardcoded string values

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,7 @@ import { useEffect } from 'react';
 import { onAuthStateChanged } from 'firebase/auth';
 import { useDispatch } from 'react-redux';
 import { setUser, clearUser } from '../store/userSlice.js';   
+import { LOGO_URL, PROFILE_PLACEHOLDER } from '../utils/constant.js';
 
 
 function Header() {
@@ -54,9 +55,9 @@ function Header() {
     return (
         <div className='absolute top-0 pt-0.5 pl-24 z-10 w-full flex  items-center justify-between bg-gradient-to-b from-black'>
             <img className='w-48 h-20 object-contain ' 
-            src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-08-26/consent/87b6a5c0-0104-4e96-a291-092c11350111/0198e689-25fa-7d64-bb49-0f7e75f898d2/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="Logo" />
+            src={LOGO_URL} alt="Logo" />
             {user && (<div className='flex items-center space-x-4 mr-8 cursor-pointer'>
-                <img className='w-10 h-10' src="https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.jpg" alt="userLogo" />
+                <img className='w-10 h-10' src={PROFILE_PLACEHOLDER} alt="userLogo" />
                 <button onClick={handleSignOut} className='text-white hover:underline'>Sign Out</button>
             </div>)}
         </div>
